491aee80e8fe82ba2ea277f9ce5aabe1c053f11a
dev <binteimran853@gmail.com>
2026-03-25 16:44:52 +0500

    add products images

---
+++ cart/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from cart.models import Cart, CartItem
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user')
+
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product_name', 'product_price','product_category', 'quantity')
+    list_filter = ('cart__user__username','product__category')
+    search_fields = ('cart', 'product__category',)
+
+    @admin.display(description='Price')
+    def product_price(self, obj):
+        return obj.product.price
+
+    @admin.display(description='Category')
+    def product_category(self, obj):
+        return obj.product.category
+
+    @admin.display(description='Name')
+    def product_name(self, obj):
+        return obj.product.name
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem, CartItemAdmin)
---
+++ cart/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .models import Cart, CartItem
+from products.models import Product
+
+
+@login_required(login_url='login')
+def user_cart(request):
+    cart, create_cart = Cart.objects.get_or_create(user=request.user)
+    items = CartItem.objects.filter(cart=cart, quantity__gt=0)
+    total_price = sum(item.quantity * item.product.price for item in items)
+    return render(
+        request, 'cart.html', {
+            'cart': cart, 'items': items, 'total_price': total_price
+        })
+
+
+@login_required(login_url='login')
+def add_cart(request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        cart, create_cart = Cart.objects.get_or_create(user=request.user)
+        cart_item, create_cart_item = CartItem.objects.get_or_create(
+            cart=cart, product=product
+        )
+        if not create_cart_item:
+            cart_item.quantity += 1
+        else:
+            cart_item.quantity = 1
+        cart_item.save()
+        messages.success(request, 'Items added successfully !!')
+        if request.method == 'POST':
+            return redirect(request.META.get('HTTP_REFERER'), 'home')
+        return redirect('cart')
+
+
+@login_required(login_url='login')
+def update_cart(request, item_id, action):
+    if request.method == 'POST':
+        cart_item = get_object_or_404(CartItem, pk=item_id)
+        if action == 'increase':
+            cart_item.quantity += 1
+        elif action == 'decrease':
+            cart_item.quantity -= 1
+        CartItem.objects.filter(quantity__lte=0).delete()
+        cart_item.save()
+    return redirect(request.META.get('HTTP_REFERER', '/cart/'))
